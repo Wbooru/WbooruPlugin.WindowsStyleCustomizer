8abf0ee67a9346f7f4da1539a9c5e2baeebab25f
Mikira Sora <mikirasora0409@126.com>
2020-01-16 13:02:56 +0800

    init commit

---
+++ WindowsStyleCustomizerPluginInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wbooru.PluginExt;
+
+namespace WbooruPlugin.WindowsStyleCustomizer
+{
+    public class WindowsStyleCustomizerPluginInfo : PluginInfo
+    {
+        public override string PluginName => "Windows Style Customizer";
+
+        public override string PluginProjectWebsite => "https://github.com/Wbooru/WbooruPlugin.WindowsStyleCustomizer";
+
+        public override string PluginAuthor => "MikiraSora";
+
+        public override string PluginDescription => "能够提供将图片设置成桌面壁纸或者锁屏壁纸的功能";
+    }
+}
